f2350fcba68070c5dbee19cf77e48ae0abb5edf9
jovan <jamorgan1@aggies.ncat.edu>
2021-07-12 18:46:10 -0400

    Another commit

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', checkAuthenticated, (req, res) => {
-    res.render('index.ejs', {name: req.user.firstname})
+    res.render('index.ejs', {fname: req.user.firstname, lname: req.user.lastname, skill: req.user.skill, address: req.user.address, phone: req.user.phone, email: req.user.email})
 })
 
 function checkAuthenticated(req, res, next) {
